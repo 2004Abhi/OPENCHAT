9e0323e9dca5b37814571e25999abacb020dcc6c
2004Abhi <abgbjb2004@gmail.com>
2023-05-31 14:26:21 +0530

    multiple chat error resolved

--- client/src/components/chatFooter.js
+++ client/src/components/chatFooter.js
@@ -1,39 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import checkPageStatus from "../utils/functions";
 import axios from "axios";
 
 const ChatFooter = ({ socket }) => {
   const [message, setMessage] = useState("");
-  let timer,
-    timeoutVal = 1000;
+  const [typingTimeout, setTypingTimeout] = useState(null);
+
   const handleTyping = () => {
-    window.clearTimeout(timer);
     socket.emit("typing", {
       name: localStorage.getItem("userName"),
       socketID: socket.id,
     });
   };
+
   const handleKeyUp = () => {
-    window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
-    timer = window.setTimeout(() => {
-        socket.emit("stoppedTyping",true);
-    }, timeoutVal);
+    clearTimeout(typingTimeout); // Clear the previous timeout
+
+    setTypingTimeout(setTimeout(() => {
+      socket.emit("stoppedTyping", true);
+    }, 1000));
   };
+
   const handleSendMessage = (e) => {
     e.preventDefault();
     if (message.trim() && localStorage.getItem("userName")) {
-      const data={
+      const data = {
         text: message,
         name: localStorage.getItem("userName"),
         id: `${socket.id}${Math.random()}`,
         socketID: socket.id,
-      }
-      axios.post(`${process.env.REACT_APP_BASE_URL}/api/chat`,data);
+      };
+
+      console.log(`This message is sent by me: ${data.name}`);
+
+      axios.post(`${process.env.REACT_APP_BASE_URL}/api/chat`, data);
       socket.emit("message", data);
-      checkPageStatus(message,localStorage.getItem("userName"))
+      checkPageStatus(message, localStorage.getItem("userName"));
     }
     setMessage("");
   };
+
+  useEffect(() => {
+    return () => {
+      // Clean up the timeout when the component unmounts
+      clearTimeout(typingTimeout);
+    };
+  }, [typingTimeout]);
+
   return (
     <div className="chat__footer">
       <form className="form" onSubmit={handleSendMessage}>
